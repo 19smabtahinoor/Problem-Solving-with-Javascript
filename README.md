628f4b1fd5037f854397fa8d46c72c74edeb546b
19smabtahinoor <abtahinoorkabid@gmail.com>
2021-06-29 21:15:22 +0600

    Problem Solved Done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+# Problem Solving
 
+Problem 1 : Remove falsy values from any array 
+Problem 2 : Convert any value to boolean
+Problem 3 : Resizing any array
+Problem 4 : How to flattern a multi-dimensional array 
+Problem 5 : Short Conditionals
+Problem 6 : Replace all occurances of a string
+Problem 7 : Know to task perfermance
--- app.js
+++ app.js
@@ -16,3 +16,49 @@
     console.log(!!1) //true
     console.log(!!0) //false
     console.log(!!undefined) //false
+
+
+//Problem 3 : Resizing any array
+    let animals = ['🐅','🐆','🐈','🐒','🦄']
+    animals.length = 3
+    console.log(animals) //['🐅','🐆','🐈']
+
+
+// Problem 4 : How to flattern a multi-dimensional array 
+
+    let smileys = ['😂',['🤣','😃','😍','😋','😚','😑','😒'],'😕','🥵','😡','🤬','🤒']
+
+    //flat() function makes the array flat
+    console.log(smileys.flat()) //['😂', '🤣', '😃','😍', '😋', '😚','😑', '😒', '😕','🥵', '😡', '🤬','🤒']
+
+    //Multi level array 
+    let smileys2 = ['😂',['🤣','😃','😍',['😋','😚'],'😑','😒'],'😕','🥵','😡','🤬','🤒']
+
+    console.log(smileys2.flat(Infinity)) //['😂', '🤣', '😃','😍', '😋', '😚','😑', '😒', '😕','🥵', '😡', '🤬','🤒']
+
+// Problem 5 : Short Conditionals
+
+    const captain = "Kabiddg"
+
+    captain === "Kabid" && console.log('😍')  // if true we use &&
+    captain === "Kabid" || console.log('😡')  // if false we use ||
+
+
+//Problem 6 : Replace all occurances of a string
+
+    const quote = "React is a JS framework & this is most popular framework right now"
+
+    //Replace all occurances of 'framework' with 'library'
+    console.log(quote.replace(/framework/g,"Libray"))
+ 
+//Problem 7 : Know to task perfermance
+
+    const startTime = performance.now()
+
+    for(let i = 0; i <= 50; i++){
+        console.log(i)
+    }
+
+    const endTime = performance.now()
+
+    console.log(`Loop take ${endTime-startTime} mileseconds`)
